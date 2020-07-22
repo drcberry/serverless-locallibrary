e7739e47fd8d72f403267874949cbd2efe740c2e
drcberry <colin@drcberry.com>
2020-07-22 16:33:24 -0500

    first commit. Adding MDN express tutorial to new Github repo

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema (
+  {
+    name: {type:String, minLength:[3, 'Too short!'], maxLength:100, required:[true, 'Must enter a genre...']}
+  }
+);
+
+//Virtual for Genre URL
+GenreSchema
+.virtual('url')
+.get(function() {
+  return './genre' + this._id;
+});
+
+module.exports = mongoose.model('Genre', GenreSchema);
